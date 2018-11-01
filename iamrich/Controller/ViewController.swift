759ee45c6d9ff69ce91694792badd6172b3e5ebb
jlm <developmob@gmail.com>
2018-11-01 19:22:37 +0100

    Initial Commit

---
+++ iamrich/Controller/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  iamrich
+//
+//  Created by LoPeZj on 01/11/2018.
+//  Copyright © 2018 developmob. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Hola que tal")
+    }
+
+
+}
+
