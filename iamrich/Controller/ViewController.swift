0dba695c493b093f044046737046ffef75b092a8
jlm <developmob@gmail.com>
2018-11-09 19:43:37 +0100

    Punto intermedio. Falta iconos, logo app y final curso.

--- iamrich/Controller/ViewController.swift
+++ iamrich/Controller/ViewController.swift
@@ -9,13 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //Properties
 
+    @IBOutlet weak var labelTittle: UILabel!
+    @IBOutlet weak var imageRuby: UIImageView!
+    @IBOutlet weak var buttonRico: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("Hola que tal!")
     }
 
-
+    //Methods
+    
+    @IBAction func pushButton(_ sender: UIButton) {
+        print("Boton pulsado")
+        let controller =  UIAlertController(title: "I am Rich", message: """
+                                                            Soy rico,
+                                                            Todo va bien,
+                                                            no hace falta mas,
+                                                            solo salud.
+                                                            """, preferredStyle: .alert)
+        
+        //let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        let action = UIAlertAction(title: "OK", style: .default) { (action) in
+            print("PULSADO OK")
+        }
+        controller.addAction(action)
+        let action1 = UIAlertAction(title: "Borrar", style: .destructive) { _ in
+            print("PULSADO Destructive")
+        }
+        controller.addAction(action1)
+       //let cancel = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: {(action) in
+            print("PULSADO CANCEL")
+        })
+        controller.addAction(cancel)
+        
+        self.show(controller, sender: nil)
+    }
+    
 }
 
